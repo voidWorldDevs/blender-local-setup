e4cddc7b7b92a277a2eed60b814d39442e9c9345
1stWorldMaster <pranaythapakoffical@gmail.com>
2025-11-20 19:28:27 +0530

    update: minor fix added install command to create the binaries

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ WORKING_DIR="~/projects/blender/blender-explore"
 cd "$WORKING_DIR"
 cmake -B ../build -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
 cd ../build
-ninja
+ninja 
+ninja install
 
 cp compile_commands.json ../blender-explore/
